2f369f6e6ab190644dbd2f355f2518a26ce64f6e
Anuj Kumar Mishra <144154519+iamakmishra11@users.noreply.github.com>
2024-09-26 20:57:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import path from 'path';
+/* eslint-disable no-undef */
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+
+
+
+
+
+
+
+
+
+// // vite.config.js
+// import { defineConfig } from 'vite';
+// import path from 'path';
+
+// export default defineConfig({
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src'),
+//     },
+//   },
+// });
